e1ad8e6b613328af46105a3a70a4af39698aa729
dmitriy <d.12.09.2002.d@gmail.com>
2022-10-14 17:14:35 +0300

    выполнил все задачи

--- src/book/book.entity.ts
+++ src/book/book.entity.ts
@@ -3,6 +3,7 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
 export class Book {
 	@PrimaryGeneratedColumn()
+	@PrimaryGeneratedColumn({name: "book_id" })
 	bookId?: number;
 
 	@Column()
